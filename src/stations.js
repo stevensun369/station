8b12f9a0b40fb6383ddbbea754336e358fb5a4d8
Steven <stevensuncatalin369@gmail.com>
2021-05-04 11:02:30 +0300

    Added a couple more stations

--- src/stations.js
+++ src/stations.js
@@ -7,5 +7,12 @@ export default {
   blues: `${protocol}live.rockfm.ro/blues`,
   kissfm: `${protocol}live.rockfm.ro/kissfm.aacp`,
   'kiss.energy': `${protocol}live.rockfm.ro/kiss.energy`,
+  'kiss.latino': `${protocol}live.rockfm.ro/kiss.latino`,
   asha: `${protocol}live.rockfm.ro/asha`,
+  one: `${protocol}live.rockfm.ro/onefm.aacp`,
+  europafm: `${protocol}astreaming.eri.ro:8443/EuropaFM_aac`,
+  vibefm: `${protocol}astreaming.eri.ro:8443/VibeFM_aac`,
+  virginradio: `${protocol}astreaming.eri.ro:8443/VirginRadio_aac`,
+  profm: `${protocol}edge126.rcs-rds.ro/profm/profm.mp3`,
+  magicfm: `${protocol}live.magicfm.ro/magicfm.aacp`,
 }
